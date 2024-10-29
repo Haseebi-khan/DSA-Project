4eb1ba5a2dd3154b1ed4872c56e532cf9e148578
Haseeb Khan <haseebkhanbettani@gmail.com>
2024-10-30 01:28:09 +0500

    Linear Search

---
+++ 16LinearSeach.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int element)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (element == arr[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+
+    int size = 10;
+    int arr[size] = {34, 23, 1, 44, 5, 67, 23, 33, 23, 5};
+
+    cout << "Enter Element for Search: ";
+    int element;
+    cin >> element;
+
+    int found = linearSearch(arr, size, element);
+
+    if (found != -1)
+    {
+        cout << found;
+    }
+    else
+    {
+        cout << "Value is not found.";
+    }
+
+    cout << endl;
+    return 0;
+}
