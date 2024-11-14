727c2dbf141c74ed603fe9d98ea86760538a7926
Haseeb Khan <haseebkhanbettani@gmail.com>
2024-11-15 00:50:00 +0500

    15.b.StackDynamic.cpp

---
+++ 15.b.StackDynamic.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int value = 0) : data(value), next(nullptr) {}
+};
+
+Node *Stacktop = nullptr;
+
+void insert(int value)
+{
+    Node *newNode = new Node(value);
+    if (Stacktop == nullptr)
+    {
+        Stacktop = newNode;
+    }
+    else
+    {
+        newNode->next = Stacktop;
+        Stacktop = newNode;
+    }
+}
+
+void insertAtEnd(int value)
+{
+    Node *newNode = new Node(value);
+    if (Stacktop == nullptr)
+    {
+        Stacktop = newNode;
+    }
+    else
+    {
+        Node *temp = Stacktop;
+        while (temp->next != nullptr)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+void display()
+{
+    Node *temp = Stacktop;
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << "\n";
+}
+
+int main()
+{
+
+    insert(10);
+    insert(20);
+    insertAtEnd(30);
+    insertAtEnd(40);
+    display(); // Expected output: 20 10 30 40
+    cout << endl;
+    return 0;
+}
