c9d52099586757b1e7faa68e2ce3b5c6a86e48f6
haseeb khan <haseebkhanbettani@gmail.com>
2024-11-18 12:03:30 +0500

    19a and 19b

--- 19.a.Queue.cpp
+++ 19.a.Queue.cpp
@@ -1,38 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int queue[5], n = 5, front = -1, rear = -1;
+const int n = 5;
+int queue[n], front = -1, rear = -1;
 
-void Insert()
+void Enqueue(int value)
 {
-    int val;
     if (rear == n - 1)
     {
-        cout << "Queue Overflow" << endl;
+        cout << "Queue Overflow." << endl;
     }
     else
     {
-
         if (front == -1)
         {
-            front = 0;
+            front++;
         }
-        cout << "Insert the element in queue : ";
-        cin >> val;
         rear++;
-        queue[rear] = val;
+        queue[rear] = value;
     }
 }
-void Delete()
+
+void Dequeue()
 {
     if (front == -1 || front > rear)
     {
-        cout << "Queue Underflow ";
-        return;
+        cout << "Queue Underflow." << endl;
     }
     else
     {
-        cout << "Element deleted from queue is : " << queue[front] << endl;
+        cout << "Dequeued: " << queue[front] << endl;
         front++;
         if (front > rear)
         {
@@ -40,49 +37,143 @@ void Delete()
         }
     }
 }
+
 void Display()
 {
     if (front == -1)
         cout << "Queue is empty" << endl;
     else
     {
-        cout << "Queue elements are : ";
-        for (int i = front; i <= rear; i++)
+        cout << "Queue elements are: ";
+        for (int i = front; i <= rear; i++) // Fix here
+        {
             cout << queue[i] << " ";
+        }
         cout << endl;
     }
 }
+
 int main()
 {
-
-    Insert();
-    Insert();
-    Insert();
-    Insert();
-    Insert();
-    Insert();
+    Enqueue(45);
+    Enqueue(3);
+    Enqueue(2);
+    Enqueue(1);
+    Enqueue(9);
+    Enqueue(7); // This will cause "Queue Overflow" because the queue size is 5
 
     Display();
 
-    Delete();
-    Delete();
-    Delete();
-    Delete();
-    Delete();
-    Delete();
+    Dequeue();
+    Dequeue();
+    Dequeue();
+    Dequeue();
+    Dequeue();
+    // Dequeuing here will result in "Queue Underflow" after all elements are removed
+    Dequeue();
+    Dequeue();
 
     cout << "\nFront: " << front << ", Rear: " << rear << ".\n";
 
-    Insert();
-    Insert();
-    Insert();
-    Insert();
-    Insert();
-    Insert();
+    Enqueue(56);
+    Enqueue(44);
+    Enqueue(33);
+    Enqueue(22);
+    Enqueue(11);
+    Enqueue(5); // This will cause "Queue Overflow" since the queue size is 5
 
     cout << "\n";
-
     Display();
+    cout << endl;
 
     return 0;
 }
+
+// #include <iostream>
+// using namespace std;
+
+// int queue[5], n = 5, front = -1, rear = -1;
+
+// void Insert()
+// {
+//     int val;
+//     if (rear == n - 1)
+//     {
+//         cout << "Queue Overflow" << endl;
+//     }
+//     else
+//     {
+
+//         if (front == -1)
+//         {
+//             front = 0;
+//         }
+//         cout << "Insert the element in queue : ";
+//         cin >> val;
+//         rear++;
+//         queue[rear] = val;
+//     }
+// }
+// void Delete()
+// {
+//     if (front == -1 || front > rear)
+//     {
+//         cout << "Queue Underflow ";
+//         return;
+//     }
+//     else
+//     {
+//         cout << "Element deleted from queue is : " << queue[front] << endl;
+//         front++;
+//         if (front > rear)
+//         {
+//             front = rear = -1;
+//         }
+//     }
+// }
+// void Display()
+// {
+//     if (front == -1)
+//         cout << "Queue is empty" << endl;
+//     else
+//     {
+//         cout << "Queue elements are : ";
+//         for (int i = front; i <= rear; i++)
+//             cout << queue[i] << " ";
+//         cout << endl;
+//     }
+// }
+// int main()
+// {
+
+//     Insert();
+//     Insert();
+//     Insert();
+//     Insert();
+//     Insert();
+//     Insert();
+
+//     Display();
+
+//     Delete();
+//     Delete();
+//     Delete();
+//     Delete();
+//     Delete();
+//     Delete();
+
+//     cout << "\nFront: " << front << ", Rear: " << rear << ".\n";
+
+//     Insert();
+//     Insert();
+//     Insert();
+//     Insert();
+//     Insert();
+//     Insert();
+
+//     cout << "\n";
+
+//     Display();
+
+//     return 0;
+// }
--- 19.b.Enqueue&Dequeue.cpp
+++ 19.b.Enqueue&Dequeue.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-class Queue {
+class Queue
+{
 private:
     int *arr;
     int front;
@@ -9,73 +10,111 @@ private:
     int capacity;
 
 public:
-    Queue(int size) {
+    // Constructor
+    Queue(int size)
+    {
         arr = new int[size];
         capacity = size;
-        front = 0;
+        front = -1;
         rear = -1;
     }
 
-    ~Queue() {
+    // Destructor
+    ~Queue()
+    {
         delete[] arr;
     }
 
-    void enqueue(int x) {
-        if (isFull()) {
+    // Enqueue an element
+    void enqueue(int x)
+    {
+        if (isFull())
+        {
             cout << "Queue is Full.\n";
             return;
         }
-        arr[++rear] = x;
+
+        if (front == -1)  // Queue is empty, set front to 0
+        {
+            front = 0;
+        }
+
+        arr[++rear] = x;  // Add element to the rear of the queue
         cout << x << " added to the queue.\n";
     }
 
-    int dequeue() {
-        if (isEmpty()) {
+    // Dequeue an element
+    int dequeue()
+    {
+        if (isEmpty())
+        {
             cout << "Queue is Empty.\n";
             return -1;
         }
-        int removed = arr[front++];
+
+        int removed = arr[front++];  // Remove element from the front of the queue
+        if (front > rear)  // Queue is empty after dequeue
+        {
+            front = rear = -1;  // Reset the queue
+        }
         cout << removed << " removed from the queue.\n";
         return removed;
     }
 
-    int getFront() {
-        if (isEmpty()) {
+    // Get the front element
+    int getFront()
+    {
+        if (isEmpty())
+        {
             cout << "Queue is empty.\n";
             return -1;
         }
         return arr[front];
     }
 
-    int getRear() {
-        if (isEmpty()) {
+    // Get the rear element
+    int getRear()
+    {
+        if (isEmpty())
+        {
             cout << "Queue is empty.\n";
             return -1;
         }
         return arr[rear];
     }
 
-    bool isEmpty() {
-        return front > rear;
+    // Check if the queue is empty
+    bool isEmpty()
+    {
+        return front == -1 || front > rear;
     }
 
-    bool isFull() {
+    // Check if the queue is full
+    bool isFull()
+    {
         return rear == capacity - 1;
     }
 };
 
-int main() {
+int main()
+{
     Queue queue(5);
 
     queue.enqueue(10);
     queue.enqueue(20);
     queue.enqueue(30);
+    queue.enqueue(40);  // Add more elements
+    queue.enqueue(50);
+    queue.enqueue(60);  // This will show "Queue is Full"
 
     cout << "Front element is: " << queue.getFront() << endl;
     cout << "Rear element is: " << queue.getRear() << endl;
 
     queue.dequeue();
     queue.dequeue();
+
+    cout << "Front element after dequeue is: " << queue.getFront() << endl;
+    cout << "Rear element after dequeue is: " << queue.getRear() << endl;
 
     return 0;
 }
