27a83008eeeaaa479b49fce4fe1f82ed7c72cbf2
haseeb khan <haseebkhanbettani@gmail.com>
2025-02-03 04:04:40 +0500

    Heap Sorting.

--- 45.c.HeapSortingAlgorithm.cpp
+++ 45.c.HeapSortingAlgorithm.cpp
@@ -1,164 +1,74 @@
 #include <iostream>
-
-using namespace std;
-
-#include <iostream>
-#include <math.h>
-#include <limits.h>
+#include <cmath>
+#include <climits>
 using namespace std;
 
 class MinHeap
 {
 private:
+public:
     int *harr;
     int capacity;
-
-public:
     int size;
-
-    MinHeap(int cap) : capacity(cap), size(0)
-    {
-        harr = new int[cap];
-    }
-    void linearSearch(int Value)
-    {
-        bool found = false;
-        for (int i = 0; i < size; i++)
-        {
-            if (harr[i] == Value)
-            {
-                found = true;
-                cout << "Value Found!\n";
-                return;
-            }
-        }
-
-        cout << "Value Not Found!\n";
-    }
-    void printArry()
-    {
-        for (int i = 0; i < size; i++)
-        {
-            cout << harr[i] << " ";
-        }
-    }
 
     int parent(int i)
     {
-        return harr[(i - 1) / 2];
+        return (i - 1) / 2;
     }
-
     int left(int i)
     {
-        return harr[(2 * i) + 1];
+        return 2 * i + 1;
     }
     int right(int i)
     {
-        return harr[(2 * i) + 2];
+        return 2 * i + 2;
     }
+
     void swap(int &x, int &y)
     {
         int temp = x;
         x = y;
         y = temp;
     }
-    int height(int i)
+
+    MinHeap(int cap) : capacity(cap), size(0)
     {
-        return ceil(log2(size + 1)) - 1;
+        harr = new int[cap];
     }
 
-    void insert(int value)
+    void unSortInsert(int value)
     {
-        if (size == capacity)
+        if (size >= capacity)
         {
-            cout << "Heap OverFlow.\n";
+            cout << "Heap Overflow\n";
             return;
         }
-
+        harr[size] = value;
         size++;
-        int i = size - 1;
-        harr[i] = value;
-
-        while (i != 0 && harr[parent(i)] > harr[i])
-        {
-            swap(harr[parent(i)], harr[i]);
-            i = parent(i);
-        }
     }
+
     void minHeapify(int i)
     {
-        int l = left(i), r = right(i), smallest = i;
-        if (i < size && harr[l] < harr[i])
-        {
+        int l = left(i);
+        int r = right(i);
+        int smallest = i;
+
+        if (l < size && harr[l] < harr[smallest])
             smallest = l;
-        }
         if (r < size && harr[r] < harr[smallest])
-        {
             smallest = r;
-        }
+
         if (smallest != i)
         {
             swap(harr[i], harr[smallest]);
             minHeapify(smallest);
         }
     }
-    int extractMin()
-    {
-        if (size <= 0)
-        {
-            cout << "Heap is Empty.\n";
-            return INT_MAX;
-        }
-        else if (size == 1)
-        {
-            size--;
-            return harr[0];
-        }
 
-        int root = harr[0];
-        harr[0] = harr[size - 1];
-        size--;
-        minHeapify(0);
-        return root;
-    }
-    void deleteKey(int i)
-    {
-        decreaseKey(i, INT_MIN);
-        extractMin();
-    }
-    void decreaseKey(int i, int newValue)
-    {
-        harr[i] = newValue;
-        while (i != 0 && harr[parent(i) > harr[i]])
-        {
-            swap(harr[i], harr[parent(i)]);
-            i = parent(i);
-        }
-    }
-    int getMini(int i)
-    {
-        return harr[0];
-    }
-    int getMax(int i)
-    {
-        return harr[size - 1];
-    }
-
-    // void HeapSoting()
-    // {
-    //     int temp[capacity];
-    //     for (int j = 0; j < capacity; j++)
-    //     {
-    //         temp[j] = extractMin();
-    //         cout << temp[j];
-    //     }
-    // }
-
-    // MinHeap::HeapSorting() corrected implementation
     void HeapSorting()
     {
-        int *temp = new int[size]; // Use dynamic array
-        int originalSize = size;   // Preserve original size
+        int originalSize = size;
+        int *temp = new int[originalSize];
 
         for (int j = 0; j < originalSize; j++)
         {
@@ -166,7 +76,36 @@ public:
             cout << temp[j] << " ";
         }
 
-        delete[] temp; // Free memory
+        delete[] temp;
+    }
+
+    int extractMin()
+    {
+        if (size <= 0)
+            return INT_MAX;
+        int root = harr[0];
+        harr[0] = harr[size - 1];
+        size--;
+        minHeapify(0);
+        return root;
+    }
+
+    void printArry()
+    {
+        for (int i = 0; i < size; i++)
+            cout << harr[i] << " ";
+        cout << "\n";
+    }
+
+    // Fix decreaseKey syntax
+    void decreaseKey(int i, int newValue)
+    {
+        harr[i] = newValue;
+        while (i != 0 && harr[parent(i)] > harr[i])
+        {
+            swap(harr[i], harr[parent(i)]);
+            i = parent(i);
+        }
     }
 };
 
@@ -174,56 +113,26 @@ int main()
 {
     MinHeap heap(10);
 
-    heap.insert(7);
-    heap.insert(4);
-    heap.insert(3);
-    heap.insert(9);
-    heap.insert(8);
-    heap.printArry();
-    cout << "\n\n";
+    heap.unSortInsert(7);
+    heap.unSortInsert(4);
+    heap.unSortInsert(3);
+    heap.unSortInsert(9);
+    heap.unSortInsert(8);
 
-    for (int i = 10 / 5 - 1; i >= 0; i--)
+    cout << "Initial Array: ";
+    heap.printArry();
+
+    for (int i = (heap.size / 2) - 1; i >= 0; i--)
     {
         heap.minHeapify(i);
     }
 
+    cout << "After Heapify: ";
+    heap.printArry();
+
+    cout << "Sorted Order: ";
     heap.HeapSorting();
 
-    cout << endl;
+    cout << "\n\n\n";
     return 0;
 }
-
-// #include <iostream>
-// #include "45.a.MinHeapDataSture.h"
-
-// using namespace std;
-
-// int main()
-// {
-//     MinHeap heap(10);
-
-//     heap.insert(7);
-//     heap.insert(4);
-//     heap.insert(3);
-//     heap.insert(9);
-//     heap.insert(8);
-
-//     cout << "Initial Heap: ";
-//     heap.printArry();
-
-//     // Build heap correctly
-//     for (int i = (heap.size / 2) - 1; i >= 0; i--)
-//     {
-//         heap.minHeapify(i);
-//     }
-
-//     cout << "After Heapify: ";
-//     heap.printArry(); // Output: 3 4 7 9 8
-
-//     cout << "Sorted Order: ";
-//     heap.HeapSorting(); // Output: 3 4 7 8 9
-
-//     cout << "\n";
-
-//     return 0;
-// }
